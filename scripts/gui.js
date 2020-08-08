3fcfccc0ec6e6582d415600604fb091bbfe4bc23
IronHeart7334 <greengrappler12@gmail.com>
2020-08-08 16:09:57 -0700

    lol spinny stonks man meme

--- scripts/gui.js
+++ scripts/gui.js
@@ -11,6 +11,14 @@ class Canvas {
         }
         this.elementSel = $(id);
         this.draw = this.elementSel.get(0).getContext("2d");
+        //this.elementSel
+        /*
+        $(window).scroll((scrollEvent)=>{
+            console.log(scrollEvent);
+            this.draw.scale(0.1, 0.1);
+        }); // for some reason this isn't firing when applied to elementSel, but it works manually
+        */
+        this.spinFactor = 0;
     }
 
     setImg(path){
@@ -18,6 +26,12 @@ class Canvas {
         this.img = new Image();
         this.img.onload=this.repaint.bind(this);
         this.img.src=path;
+    }
+
+    spin(){
+        this.draw.setTransform(Math.cos(this.spinFactor), Math.sin(this.spinFactor), -Math.sin(this.spinFactor), Math.cos(this.spinFactor), 0, 0);
+        this.spinFactor+=1;
+        this.repaint();
     }
 
     repaint(){
@@ -32,6 +46,12 @@ function linkGui(options={}){
 
 }
 
-new Canvas("map").setImg("./data/5f3.jpg");
+let canvas = new Canvas("map");
+canvas.setImg("./data/5f3.jpg");
+let i = setInterval(()=>{
+    console.log(canvas);
+    canvas.spin();
+}, 10);
+setTimeout(()=>clearInterval(i), 360 * 10);
 
 export default linkGui;
