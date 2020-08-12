b1c32a72ef00d537e4348bd07dee8132c64d9197
IronHeart7334 <greengrappler12@gmail.com>
2020-08-12 15:27:35 -0700

    csv.js isn't done, but I'll need other files done before I can test it properly

---
+++ scripts/csv.js
@@ -0,0 +1,32 @@
+/*
+This file is used to convert fetch response text to CsvFile objects.
+*/
+
+class CsvFile {
+    constructor(headers){
+        this.headerRow = [];
+        this.headerToCol = new Map();
+        this.body = [];
+
+        headers.map((header)=>{
+            return header.toUpperCase();
+        }).forEach((header)=>{
+            if(!this.headerToCol.has(header)){
+                this.headerToCol.set(header, this.headerRow.length);
+                this.headerRow.push(header);
+            }
+        });
+    }
+
+    getBody(){
+        return this.body;
+    }
+}
+
+/*
+responseText: string
+returns CsvFile
+*/
+function parseResponseText(responseText){
+
+}
