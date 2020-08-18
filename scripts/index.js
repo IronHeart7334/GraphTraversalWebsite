1093d8df2ac6fd536bcfecc133987beb3fc9c5be
IronHeart7334 <greengrappler12@gmail.com>
2020-08-18 09:48:30 -0700

    Yup! Looks like it also works for larger data sets! May try it with some of the campus data once I add graph rendering

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,8 @@ const VERSION_LOG_URL = "../data/versionLog.csv";
 let graph = new Graph();
 
 //downloadFile("../data/testCoords.csv").then((t)=>console.log(t));
-getLatestGraph(VERSION_LOG_URL, "default", graph).then(()=>{
+getLatestGraph(VERSION_LOG_URL, "wayfinding", graph).then(()=>{
     console.log(graph.prettyPrintGraphData());
-    console.log(graph.findPath(0, 3).toString());
+    console.log(graph.findPath(0, 7).toString());
 });
 //downloadFile("../data/testData.csv").then((text)=>console.log(parseResponseText(text).toString()));
